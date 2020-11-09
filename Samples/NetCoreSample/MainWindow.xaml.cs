925c7b834be1f896179fb04e4c74856f490d7481
evaristocuesta <evaristocg@gmail.com>
2020-11-09 17:43:55 +0100

    New .NET Core Sample project added.

---
+++ Samples/NetCoreSample/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using FolderBrowserEx;
+using System.Windows;
+
+namespace NetCoreSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel(new FolderBrowserDialog());
+        }
+    }
+}
---
+++ Samples/NetCoreSample/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using FolderBrowserEx;
+using MVVMBase;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace NetCoreSample
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly IFolderBrowserDialog _folderBrowserDialog;
+        private string _result;
+
+        public MainWindowViewModel(IFolderBrowserDialog folderBrowserDialog)
+        {
+            _folderBrowserDialog = folderBrowserDialog;
+            ShowFolderBrowserCommand = new Command(ShowFolderBrowserCommandExecute, ShowFolderBrowserCommandCanExecute);
+        }
+
+        public ICommand ShowFolderBrowserCommand { get; private set; }
+
+        public string Result
+        {
+            get { return _result; }
+            set { _result = value; OnPropertyChanged(); }
+        }
+
+        private bool ShowFolderBrowserCommandCanExecute()
+        {
+            return true;
+        }
+
+        private void ShowFolderBrowserCommandExecute()
+        {
+            _folderBrowserDialog.Title = "Select a folder";
+            _folderBrowserDialog.InitialFolder = @"C:\";
+            if (_folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                Result += $"{_folderBrowserDialog.SelectedFolder}\n";
+            }
+        }
+    }
+}
